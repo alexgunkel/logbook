99181e5448c50a1937841e7d6b50e3f847c4788f
Alexander Gunkel <alexandergunkel@gmail.com>
2017-11-22 12:03:04 +0100

    Use gin framework

--- logbook.go
+++ logbook.go
@@ -1,9 +1,36 @@
 package logbook
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
 
-func DisplayLogBook(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-type", "application/json")
-	cookie := &http.Cookie{Name: "name", Value: "value"}
-	http.SetCookie(w, cookie)
+const CONTENT_TYPE_JSON  = "application/json"
+
+type logEvent struct {
+	m string
+}
+
+func SetUpRouter() *gin.Engine {
+	engine := gin.Default()
+	engine.GET("/display", DisplayLogBook)
+	engine.POST("/logbook/:client/logs", Log)
+
+	return engine
+}
+
+func DisplayLogBook(c *gin.Context)  {
+	c.SetCookie("logbook", "asd", 0, "", "", false, false)
+	c.Header("Content-Type", CONTENT_TYPE_JSON)
+	c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+}
+
+func Log(c *gin.Context)  {
+	e := &logEvent{}
+	err := c.Bind(e)
+	if nil != err {
+		return
+	}
 }
--- logbook_test.go
+++ logbook_test.go
@@ -2,32 +2,51 @@ package logbook
 
 import (
 	"net/http"
-	"testing"
 	"net/http/httptest"
+	"testing"
+	"encoding/json"
+	"strings"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestDisplayLogBook(t *testing.T)  {
+func TestDisplayLogBook(t *testing.T) {
 	// Create a first request to log-display
 	request, err := http.NewRequest("GET", "/display", nil)
 	if nil != err {
 		t.Fatal(err)
 	}
 
+	router := SetUpRouter()
 	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(DisplayLogBook)
-	handler.ServeHTTP(recorder, request)
+	router.ServeHTTP(recorder, request)
 
-	if status := recorder.Code; status != http.StatusOK {
-		t.Errorf("Didn't receive status code: Got %v wanted %v", status, http.StatusOK)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t,	"application/json", recorder.Header().Get("Content-type"))
+	assert.NotEqual(t, "", recorder.Header().Get("Set-Cookie"))
+}
+
+func TestEmptyLogEventLogEvent(t *testing.T) {
+	request, err := http.NewRequest("POST", "/logbook/1234/logs", nil)
+	if nil != err {
+		t.Fatal(err)
 	}
 
-	// expect setting cookie
-	if contentType := recorder.Header().Get("Content-type"); "application/json" != contentType {
-		t.Error("No or wrong content-type set in HTTP response; expected application/json, got %q.", contentType)
+	recorder := httptest.NewRecorder()
+	router := SetUpRouter()
+	router.ServeHTTP(recorder, request)
+
+	assert.Equal(t, http.StatusBadRequest, recorder.Code)
+}
+
+func TestValidLogAccepted(t *testing.T)  {
+	router := SetUpRouter()
+	recorder := httptest.NewRecorder()
+	requestBody,_ := json.Marshal(logEvent{"Test"})
+	request, err := http.NewRequest("POST", "/logbook/12345/logs", strings.NewReader(string(requestBody)))
+	if nil != err {
+		t.Fatal(err)
 	}
 
-	// expect setting cookie
-	if "" == recorder.Header().Get("Set-Cookie") {
-		t.Error("No cookie set in HTTP response.")
-	}
+	router.ServeHTTP(recorder, request)
+	assert.Equal(t, http.StatusOK, recorder.Code)
 }
