b2a8b922844f2d2b92411d36d4d15a5060356986
Alexander Gunkel <alexandergunkel@gmail.com>
2017-11-27 20:45:22 +0100

    Add function to convert different formats of severity

--- application/logmessage.go
+++ application/logmessage.go
@@ -6,6 +6,13 @@ type Message struct {
 	Origin    Origin
 }
 
+type Incoming struct {
+	Timestamp int         `json:"timestamp"`
+	Message   string      `json:"message"`
+	Severity  interface{} `json:"severity"`
+	Context   interface{} `json:"context"`
+}
+
 type Event struct {
 	Timestamp int         `json:"timestamp"`
 	Message   string      `json:"message"`
@@ -14,14 +21,36 @@ type Event struct {
 }
 
 type Origin struct {
-	Application string
-	LoggerName  string
-	RequestUri  string
+	Application string `json:"application"`
+	LoggerName  string `json:"logger_name"`
+	RequestUri  string `json:"request_uri"`
 }
 
 func createNewLogMessage(logBookId string) (m *Message) {
 	m = &Message{}
 	m.logBookId = logBookId
+
+	return
+}
+
+func (i Incoming) normalize() (e Event) {
+	if level, ok := i.Severity.(string); ok {
+		severityValues := map[string]int{"debug": 7,
+			"informational": 6,
+			"notice": 5,
+			"warning": 4,
+			"error": 3,
+			"critical": 2,
+			"alert": 1,
+			"emergency": 0}
+		e = Event{Severity: severityValues[level]}
+		return
+	}
+
+	if level, ok := i.Severity.(float64); ok {
+		e = Event{Severity: int(level)}
+		return
+	}
 
 	return
 }
--- application/logmessage_test.go
+++ application/logmessage_test.go
@@ -7,14 +7,49 @@ import (
 )
 
 func TestUnMarshallSeverity(t *testing.T)  {
-	t.SkipNow()
 	origJson := "{\"severity\": 12}"
 	origJson2 := "{\"severity\": \"error\"}"
-	logEv := &Event{}
-	logEv2 := &Event{}
+	logEv := &Incoming{}
+	logEv2 := &Incoming{}
 	json.Unmarshal([]byte(origJson), logEv)
 	json.Unmarshal([]byte(origJson2), logEv2)
 
-	assert.Equal(t, 12, logEv.Severity)
+	assert.Equal(t, float64(12), logEv.Severity)
 	assert.Equal(t, "error", logEv2.Severity)
+}
+
+func TestNormalize(t *testing.T)  {
+	for input, output := range dataProviderForNormalization() {
+		inc := Incoming{Severity: input}
+		out := inc.normalize()
+		assert.Equal(t, output, out.Severity)
+	}
+}
+
+// According to RFC 5424 we have the following associations between numbers
+// and log devels:
+//
+// 0       Emergency: system is unusable
+// 1       Alert: action must be taken immediately
+// 2       Critical: critical conditions
+// 3       Error: error conditions
+// 4       Warning: warning conditions
+// 5       Notice: normal but significant condition
+// 6       Informational: informational messages
+// 7       Debug: debug-level messages
+//
+// see https://tools.ietf.org/html/rfc5424
+func dataProviderForNormalization() (m map[interface{}]interface{}) {
+	m = make(map[interface{}]interface{})
+	m["debug"] = 7
+	m["informational"] = 6
+	m["notice"] = 5
+	m["warning"] = 4
+	m["error"] = 3
+	m["critical"] = 2
+	m["alert"] = 1
+	m["emergency"] = 0
+	m[float64(1)] = 1
+
+	return
 }
